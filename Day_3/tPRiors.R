232fe6d0c2518e35fb2fce5d9959236594f9ed6b
kostas.pateras@gmail.com <63882794+kpateras@users.noreply.github.com>
2021-09-16 12:28:05 +0300

    Examples + further analysis

--- Day_3/tPRiors.R
+++
@@ -1,9 +0,0 @@
-#tPRiors models
-
-#tPRiors priors
-
-#tPRiors objects
-
-#tPRiors diagnostics
-
-#tPRiors further analysis
--- Day_3/tPRiors_Practical.R
+++ Day_3/tPRiors_Practical.R
@@ -1,20 +1,48 @@
-#Example - Single population with article and results
-# Understand assumptions
-# Collect data
-# Elicitate priors (based on?)
-# Input Data
-# Check init properties
-# Demonstrate report
-# Further work with exported samples
+"model{
+    y ~ dbinom(main.ap, n)                                                                                   
+    pre.y ~ dbinom(main.ap, m)                                                                                
+    #Uniform (non-informative) prior for apparent prevalence (ap)                                           
+    main.ap  ~ dbeta(a,b)
+    plessthanSetvalue <- step(perVal-main.ap)                                                                 
+  }" 
 
-#Example - Multiple population with article and results
-# Understand assumptions
-# Collect data
-# Elicitate priors (based on?)
-# Input Data
-# Check init properties
-# Demonstrate report
-# Further work with exported samples
+load("~/GitHub/harmony_larissa_training/Day_3/Example.mcmc.RData")
+# Combine multiple chains to one
+library(runjags)
+fitbined <- combine.mcmc(Model1.mcmc)
 
-install.packages("devtools")
-devtools::install_github("kpatera/tPRiors")
+library(rjags)
+fitbined<-mcmc(do.call(rbind, Model1.mcmc))
+
+
+# Further analysis 
+
+traceplot(fitbined)
+
+autocorr.plot(fitbined)
+
+geweke.plot(fitbined)
+
+require(ggmcmc)
+S2<-ggs(fitbined)
+
+ggs_histogram(S2) # Histograms (Main)
+
+ggs_density(S2,family = "main") # Density plots for main parameters
+
+ggs_density(S2,family = "sub") # Density plots for study prevalences
+
+ggs_density(S2,family = "pre") # Density plots for predictive parameters
+
+ggs_traceplot(S2,family = "main") # Trace plots (Main)
+
+ggs_running(S2,family = "main") # Running means plots (Main)
+
+ggs_compare_partial(S2,family = "main") # Partial chain comparison plots (Main)
+
+ggs_autocorrelation(S2,family = "main") # Autocorrelation plots (Main)
+
+#ggs_pairs(S2,lower = list(continuous = "density")) #crosscorrelation plot, at least 2 continuous inputs
+
+# Bayesplot - rstan 
+#https://cran.r-project.org/web/packages/bayesplot/vignettes/visual-mcmc-diagnostics.html
